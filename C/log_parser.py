cb49fca6b919de6eb88b18b2b2e2ac158a68384f
Михаил <mike.milos@yandex.ru>
2018-06-14 10:41:21 +0300

    C

--- C/log_parser.py
+++ C/log_parser.py
@@ -2,10 +2,18 @@
 # -*- coding: utf-8 -*-
 import sys 
 import os 
+import time
+import datetime
 
+def unix_time(str):
+    return time.mktime(datetime.datetime.strptime(str, "%d.%m.%Y %H:%M:%S").timetuple())
 
 if os.path.exists(sys.argv[1]) == False:
-    print ("Файл "+sys.argv[1]+" не  найден. ")
     sys.exit("Файл "+sys.argv[1]+" не  найден. ")
 
-print(len(sys.argv))
+
+f = open(sys.argv[1], 'r')
+for line in f:
+    line_arr = line.split(" | ")
+    processing = unix_time(line_arr[1]) - unix_time(line_arr[0])
+    print(processing)
