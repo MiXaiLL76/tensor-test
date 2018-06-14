88a3abdc569a473dc66a8af4d45580baa8b98b98
Михаил <mike.milos@yandex.ru>
2018-06-14 10:26:07 +0300

    B

--- C/log_parser.py
+++ C/log_parser.py
@@ -4,10 +4,8 @@ import sys
 import os 
 
 
-
-if os.path.exists(sys.argv[1]):
-    print ("'"+sys.argv[1]+"' удалён.")                    
-else: 
-    print ("'"+sys.argv[1]+"' не найден в директории "+dir)
+if os.path.exists(sys.argv[1]) == False:
+    print ("Файл "+sys.argv[1]+" не  найден. ")
+    sys.exit("Файл "+sys.argv[1]+" не  найден. ")
 
 print(len(sys.argv))
