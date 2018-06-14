e743047893145a4ae832f564249e5298752036e9
Михаил <mike.milos@yandex.ru>
2018-06-14 10:22:55 +0300

    B

--- C/log_parser.py
+++ C/log_parser.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys 
+import os 
+
+
+
+if os.path.exists(sys.argv[1]):
+    print ("'"+sys.argv[1]+"' удалён.")                    
+else: 
+    print ("'"+sys.argv[1]+"' не найден в директории "+dir)
+
+print(len(sys.argv))
